ed9aa09c807d877db90b6fe9d5494420e9a6227e
ksrzinn <franca.arth@hotmail.com>
2023-10-29 04:45:01 -0300

    Component PageTitle created

--- src/app/components/pagetitle/pagetitle.component.ts
+++ src/app/components/pagetitle/pagetitle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pagetitle',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagetitle.component.css']
 })
 export class PagetitleComponent implements OnInit {
+
+  @Input()
+  title:string = '';
 
   constructor() { }
 
